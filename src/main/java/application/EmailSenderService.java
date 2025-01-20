0a5baaa2eaa221f12faf1912ac34ec64fe69533c
Hector-Lopes <hector27.lopes@gmail.com>
2025-01-20 10:28:30 -0300

    feat: add the application of project how the function its be abstraction of core and connect with the aws service message and protect the core

---
+++ src/main/java/application/EmailSenderService.java
@@ -0,0 +1,23 @@
+package application;
+
+import adapters.EmailSenderGateway;
+import core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+private final EmailSenderGateway emailSenderGateway; //Aqui eu importo a interface EmailSenderGateway para a metodo emailSenderGateway
+
+  @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){ //aqui eu crio metodo construtor q vai receber o valor na variavel emailGateway e seto ela  no metodo q eu importei
+    this.emailSenderGateway = emailGateway;
+  }
+
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+    this.emailSenderGateway.sendEmail(to,subject,body);
+    }
+}
